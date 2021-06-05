959c8bb624c200be6098f6aebd42f873af6fedf4
leiyunkang <leiyunkang7@outlook.com>
2021-06-05 18:04:36 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
 
-createApp(App).use(Router).mount('#app')
+createApp(App).use(Router).use(ElementPlus).mount('#app')
